bc68ad1a5f659f729ab950d90f991585b35170ca
Davi Monteiro <monteiro_davi@yahoo.com>
2024-01-31 15:02:02 -0300

    Criando banco de dados no docker com postgres e conectando ao Nest

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
-
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local'],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: process.env.DB_DATABASE,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      port: Number(process.env.DB_PORT),
+      host: process.env.DB_HOST,
+    }),
+  ],
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService],
 })
 export class UserModule {}
