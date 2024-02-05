d71dd0f8df907e8b81c82fdfdcbb416e1536f710
Davi Monteiro <monteiro_davi@yahoo.com>
2024-02-05 18:28:04 -0300

    Criando a função de criar categoria utilizando TDD

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,8 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { Roles } from 'src/decorators/roles.decorator';
 import { UserType } from 'src/user/enum/user-type.enum';
 import { CategoryService } from './category.service';
 import { ReturnCategory } from './dtos/return-category.dto';
+import { CategoryEntity } from './entities/category.entity';
+import { CreateCategory } from './dtos/create-category.dto';
 @Roles(UserType.Admin, UserType.User)
 @Controller('category')
 export class CategoryController {
@@ -13,5 +22,14 @@ export class CategoryController {
     return (await this.categoryService.getAllCategories()).map(
       (category) => new ReturnCategory(category),
     );
+  }
+
+  @Roles(UserType.Admin, UserType.User)
+  @UsePipes(ValidationPipe)
+  @Post()
+  async createCategory(
+    @Body() createCategory: CreateCategory,
+  ): Promise<CategoryEntity> {
+    return this.categoryService.createCategory(createCategory);
   }
 }
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,7 +1,12 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CategoryEntity } from './entities/category.entity';
+import { CreateCategory } from './dtos/create-category.dto';
 
 @Injectable()
 export class CategoryService {
@@ -18,5 +23,35 @@ export class CategoryService {
     }
 
     return categories;
+  }
+
+  async getCategoryByName(name: string): Promise<CategoryEntity> {
+    const category = await this.categoryRepository.findOne({
+      where: {
+        name,
+      },
+    });
+
+    if (!category) {
+      throw new NotFoundException(`Category name ${name} not found`);
+    }
+
+    return category;
+  }
+
+  async createCategory(
+    createCategory: CreateCategory,
+  ): Promise<CategoryEntity> {
+    const category = await this.getCategoryByName(createCategory.name).catch(
+      () => undefined,
+    );
+
+    if (category) {
+      throw new BadRequestException(
+        `Category name ${createCategory.name} exist`,
+      );
+    }
+
+    return this.categoryRepository.save(createCategory);
   }
 }
