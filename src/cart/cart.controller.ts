4794b7deb0fe5165e3803c0a7115db35fc571a98
Davi Monteiro <monteiro_davi@yahoo.com>
2024-02-16 15:40:13 -0300

    Criando função para criar carrinho de compras do usuário

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -1,7 +1,28 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { Roles } from 'src/decorators/roles.decorator';
 import { UserType } from 'src/user/enum/user-type.enum';
+import { CartService } from './cart.service';
+import { InsertCartDto } from './dto/insert-cart.dto';
+import { UserId } from 'src/decorators/user-id.decorator';
+import { CartEntity } from './entities/cart.entity';
 
-@Roles(UserType.User)
+@Roles(UserType.User, UserType.Admin)
 @Controller('cart')
-export class CartController {}
+export class CartController {
+  constructor(private readonly cartService: CartService) {}
+
+  @UsePipes(ValidationPipe)
+  @Post()
+  async createCart(
+    @Body() insertCart: InsertCartDto,
+    @UserId() userId: number,
+  ): Promise<CartEntity> {
+    return this.cartService.insertProductInCart(insertCart, userId);
+  }
+}
