6ea4a41ddbcd44a7051cc91f4ac3a78d90298eb5
Davi Monteiro <monteiro_davi@yahoo.com>
2024-02-23 15:47:12 -0300

    Começando os testes do payment

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -3,14 +3,14 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateOrderDto } from './dtos/create-order.dto';
 import { OrderEntity } from './entities/order.entity';
-import { PaymentService } from 'src/payment/payment.service';
-import { PaymentEntity } from 'src/payment/entities/payment.entity';
-import { CartService } from 'src/cart/cart.service';
-import { OrderProductService } from 'src/order-product/order-product.service';
-import { ProductService } from 'src/product/product.service';
-import { CartEntity } from 'src/cart/entities/cart.entity';
-import { ProductEntity } from 'src/product/entities/product.entity';
-import { OrderProductEntity } from 'src/order-product/entities/order-product.entity';
+import { PaymentService } from '../payment/payment.service';
+import { PaymentEntity } from '../payment/entities/payment.entity';
+import { CartService } from '../cart/cart.service';
+import { OrderProductService } from '../order-product/order-product.service';
+import { ProductService } from '../product/product.service';
+import { CartEntity } from '../cart/entities/cart.entity';
+import { ProductEntity } from '../product/entities/product.entity';
+import { OrderProductEntity } from '../order-product/entities/order-product.entity';
 
 @Injectable()
 export class OrderService {
