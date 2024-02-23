8c78c2f618357fe8a5c407ef26ac0787e4751893
Davi Monteiro <monteiro_davi@yahoo.com>
2024-02-23 10:46:46 -0300

    Criando service do payment

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -1,4 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PaymentEntity } from './entities/payment.entity';
+import { Repository } from 'typeorm';
+import { CreateOrderDto } from 'src/order/dtos/create-order.dto';
+import { PaymentCreditCardEntity } from './entities/payment-credit-card.entity';
+import { PaymentType } from 'src/payment-status/enums/payment-type.enum';
+import { PaymentPixEntity } from './entities/payment-pix.entity';
 
 @Injectable()
-export class PaymentService {}
+export class PaymentService {
+  constructor(
+    @InjectRepository(PaymentEntity)
+    private readonly paymentRepository: Repository<PaymentEntity>,
+  ) {}
+
+  async createPayment(createOrderDTO: CreateOrderDto): Promise<PaymentEntity> {
+    if (createOrderDTO.amountPayments) {
+      const paymentCreditCard = new PaymentCreditCardEntity(
+        PaymentType.Done,
+        0,
+        0,
+        0,
+        createOrderDTO,
+      );
+      return this.paymentRepository.save(paymentCreditCard);
+    } else if (createOrderDTO.codePix && createOrderDTO.datePayment) {
+      const paymentPix = new PaymentPixEntity(
+        PaymentType.Done,
+        0,
+        0,
+        0,
+        createOrderDTO,
+      );
+      return this.paymentRepository.save(paymentPix);
+    }
+
+    throw new BadRequestException(
+      'Amount Payments or code pix or date payment not found',
+    );
+  }
+}
